a20c4ac653076727f0b20219017a785e17412bd4
Minura Hajisoy <minura.hajisoy@gmail.com>
2023-08-17 14:27:54 +0400

    AutoMapper configured

---
+++ HR.LeaveManagement.Application/DTOs/LeaveTypeDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Application.DTOs
+{
+    public class LeaveTypeDto
+    {
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+    }
+}
