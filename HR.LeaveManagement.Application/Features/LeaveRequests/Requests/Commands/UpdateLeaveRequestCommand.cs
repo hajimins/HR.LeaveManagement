7027d738e18df0ee4b07001d5cfe75d3a9552ffe
Minura Hajisoy <minura.hajisoy@gmail.com>
2023-08-18 12:37:46 +0400

    updating commands

---
+++ HR.LeaveManagement.Application/Features/LeaveRequests/Requests/Commands/UpdateLeaveRequestCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Application.Features.LeaveRequests.Requests.Commands
+{
+    public class UpdateLeaveRequestCommand : IRequest<Unit>
+    {
+    }
+}
