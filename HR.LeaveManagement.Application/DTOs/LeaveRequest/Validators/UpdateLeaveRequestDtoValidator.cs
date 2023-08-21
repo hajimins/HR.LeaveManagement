3f9a5ee2cf50062b063c8027f606982ad33ad404
Minura Hajisoy <minura.hajisoy@gmail.com>
2023-08-21 21:08:47 +0400

    solving errors

--- HR.LeaveManagement.Application/DTOs/LeaveRequest/Validators/UpdateLeaveRequestDtoValidator.cs
+++ HR.LeaveManagement.Application/DTOs/LeaveRequest/Validators/UpdateLeaveRequestDtoValidator.cs
@@ -3,6 +3,7 @@ using HR.LeaveManagement.Application.Persistence.Contracts;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace HR.LeaveManagement.Application.DTOs.LeaveRequest.Validators
 {
